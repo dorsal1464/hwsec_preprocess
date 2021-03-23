a74803c69e4fbe71f06bb534501e550412e4c7fe
Dor Salomon <56582984+dorsal1464@users.noreply.github.com>
2021-03-23 19:30:38 +0200

    Add files via upload
    config file for various settings

---
+++ config.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# config file for settings
+
+from matplotlib import cm
+
+# General
+max_workers = 30
+
+max_jobs = 30
+
+# FFT
+
+id_frames = 20
+
+best_factor = 0.6
+
+colormap = cm.jet
