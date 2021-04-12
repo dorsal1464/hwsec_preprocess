9019d93ddb0a497afdc122393743b171480e401c
dorsal1464 <dorsal1464@gmail.com>
2021-04-12 11:38:57 +0300

    PCA reimplementation, Feature selection...

--- PCA/PCA.py
+++ PCA/PCA.py
@@ -1,18 +1,36 @@
 import numpy as np
 
 
-def PCA(X, dim):
-    # standardize the data
-    # X = (X - np.mean(X)) / np.std(X)
-    # calc cov matrix
-    # cov = np.cov(X)
-    # np.linalg.eigenvec(X) np.linalg.eigenval(X)
-    # the shorter way is to SVD(X)
-    u, s, vh = np.linalg.svd(X)
-    # s are the singular values in decending order
-    # the columns of u are the eigenvectors of XX^t
-    # the rows of vh are the eigenvectors of X^t X
-    vh = np.transpose(vh)
-    # now calc vh^t so the columns are the eigenvectors of X^t X
-    return s[:dim], np.matmul(X, vh[:, :dim])
-# return (s, pca_mat)
+class PCA:
+    def __init__(self, X):
+        self.mat = X
+        self._mean = np.mean(X)
+        self._std = np.std(X)
+        self.vh = None
+
+    def fit(self, dim):
+        # standardize the data
+        mat = (self.mat - self._mean) / self._std
+        # calc cov matrix
+        cov = np.matmul(np.transpose(mat), mat)
+        s, vh = np.linalg.eig(cov)
+
+        # the shorter way is to SVD(X), but too compute. expensive
+        # u, s, vh = np.linalg.svd(X)
+        # s are the singular values in decending order
+        # the columns of u are the eigenvectors of XX^t
+        # the rows of vh are the eigenvectors of X^t X
+        # vh = np.transpose(vh)
+        # now calc vh^t so the columns are the eigenvectors of X^t X
+        return s[:dim], np.matmul(mat, vh[:, :dim])
+
+    def transform(self):
+        # standardize the data
+        mat = (self.mat - self._mean) / self._std
+        # calc cov matrix
+        cov = np.matmul(np.transpose(mat), mat)
+        s, vh = np.linalg.eig(cov)
+        return np.matmul(mat, vh)
+
+    def itransform(self, mat):
+        return np.matmul(mat, np.transpose(self.vh)) * self._std + self._mean
--- main_pca.py
+++ main_pca.py
@@ -1,29 +1,47 @@
-from PCA import PCA
+import hdf5storage
+
+from PCA.PCA import PCA
 from SNR import SNR
 from scipy.io import loadmat
 from matplotlib import pyplot as plt
 import numpy as np
 
-mat = loadmat('Z:\\traces\\pca_dom_101_1101.mat')
-traces = mat['pca']
-Y = mat['Y']
+crop = range(300, 1300)
 
-snr_t = np.abs(SNR.SNR(traces, Y, 256, 20, np.float32))
+mat = loadmat('Z:\\traces\\traces_101_601_full.mat')
+traces = mat['traces'][:, crop]
+SAMPLES = np.shape(traces)[1]
+Y = mat['Y']
+pca = PCA(traces).fit(1000)[1]
+snr_t = np.abs(SNR.SNR(pca, Y, 256, SAMPLES, np.float32))
 
 fig, ax = plt.subplots()
-ax.plot(range(0, 20), snr_t)
+ax.plot(range(0, SAMPLES), snr_t)
 ax.set_title("SNR of PCA in time")
 ax.set_xlabel("dimension-time")
 plt.show()
 
-mat = loadmat('Z:\\traces\\pca_freq_101_1101.mat')
-traces = mat['pca']
+mat = hdf5storage.loadmat('Z:\\traces\\fft_301_401.mat')
+traces = mat['fft']
 Y = mat['Y']
+SAMPLES = np.shape(traces)[1]
+pca = PCA(traces).fit(1400)[1]
 
-snr_t = np.abs(SNR.SNR(traces, Y, 256, 20, np.complex128))
+snr_t = np.abs(SNR.SNR(pca, Y, 256, SAMPLES, np.complex128))
 
 fig, ax = plt.subplots()
-ax.plot(range(0, 20), snr_t)
+ax.plot(range(0, SAMPLES), snr_t)
 ax.set_title("SNR of PCA in frequency")
 ax.set_xlabel("dimension-frequency")
+plt.show()
+
+ifft = np.fft.ifft(pca, n=1400, axis=1)
+plt.plot(ifft[1, :])
+plt.show()
+snr_t = np.abs(SNR.SNR(ifft, Y, 256, SAMPLES, np.complex128))
+
+fig, ax = plt.subplots()
+ax.plot(range(0, 1400), snr_t)
+ax.set_title("SNR of PCA in frequency with ifft")
+ax.set_xlabel("time")
 plt.show()
